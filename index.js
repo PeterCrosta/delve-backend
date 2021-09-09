4c2490c174efe5be456fbf71350b6a42f1cbd455
PeterCrosta <pscrosta@gmail.com>
2021-09-09 15:04:43 -0700

    update cors policy

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
-// const axios = require('axios');
+const cors = require('cors');
+
+app.use(cors({
+    origin: '*'
+}))
 
 
 app.use(express.json());
@@ -9,15 +13,4 @@ const apiRouter = require('./routes/api');
 
 app.use('/api', apiRouter);
 
-// app.get('/', async (req, res, next) => {
-//     try {
-//         const {data} = await axios.get("https://data.cityofnewyork.us/resource/q2z5-ai38.json");
-//         res.json(data);
-//     } catch (error) {
-//         console.error(error)
-//     }
-//     // res.send('this is just a test');
-//     // res.download('server.js'); reference for downloading in the future
-// })
-
-app.listen(3000);
+app.listen(3001);
