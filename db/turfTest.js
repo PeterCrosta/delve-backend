37ef987f0adaecdf44d46986c0880fb2cdab5d32
PeterCrosta <pscrosta@gmail.com>
2021-09-21 16:01:01 -0700

    both api calls work

--- db/turfTest.js
+++ db/turfTest.js
@@ -3,9 +3,9 @@ const axios = require('axios');
 
 const testTurf = async () => {
     try {
-        let { data }  = await axios.get('https://data.cityofnewyork.us/resource/q2z5-ai38.json?$limit=1');
-        const mapData = data;
-        console.log(mapData);
+        const { data: mapData }  = await axios.get('https://data.cityofnewyork.us/resource/q2z5-ai38.json?$limit=1');
+        const { data: complaintData } = await axios.get("https://data.cityofnewyork.us/resource/erm2-nwe9.json?$where=created_date%20%3E%20%272021-09-18T12:00:00%27")
+        console.log(complaintData);
     } catch (err) {
         console.error(err);
     }
